5acad55c7c0098f36431f308e7d944c53e27dfb5
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-16 19:08:08 +0530

    Header and Hero component changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Hero from "./Components/Hero/Hero";
 
 export default function App() {
   return (
-    <div className="bg-violet-black h-screen">
+    <div className="bg-violet-black ">
       <Header/>
       <Hero/>
+    
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import firstImg from '../../assets/Hero/firstImg.svg'
+import React from 'react';
+import firstImg from '../../assets/Hero/firstImg.svg';
+import secondImg from '../../assets/Hero/secondImg.svg';
+
 function Hero() {
-    return (
+  return (
+    <div>
+      <div className="p-4 text-white flex items-center justify-between pl-24 pr-24 bg-cover bg-center">
+        <div>
+          <h1 className="text-5xl w-full text-60 font-oxanium font-400 capitalize break-words mt-14">
+            EXPLORE, CREATE AND
+            <br /> SELL YOUR NFT’S ON
+            <br /> SHIHAMI
+          </h1>
+          <p className="text-white text-25 font-Lato font-thin capitalize break-words mt-14">
+            Shihami is the World’s first and largest NFT/Avatar marketplace
+          </p>
+
+          <button className="bg-violet w-32 h-10 text-white rounded-lg mt-28 font-oxanium">
+            Explore More
+          </button>
+          <button className="border border-white bg-transparent w-32 h-10 text-white rounded-lg ml-4 font-oxanium mt-28">
+            Create
+          </button>
+        </div>
+
+        <div>
+          <img className="w-3/4 ml-auto mt-8" src={firstImg} />
+        </div>
+      </div>
+
+      <div className="bg-cover bg-center p-28" style={{ backgroundImage: `url(${secondImg})` }}>
         
-
-            <div className='p-4 text-white flex items-center justify-between pl-16 pr-16'>
-                <div >
-                    <h1 className='text-4xl w-full text-54 font-oxanium font-400 capitalize break-words'>EXPLORE, CREATE AND<br /> SELL YOUR NFT’S ON<br /> SHIHAMI</h1>
-                    <p class="text-white text-25 font-Lato font-thin capitalize break-words mt-4  ">
-                        Shihami is the World’s first and largest NFT/Avatar marketplace
-                    </p>
-                
-                    <button className='bg-violet w-32 h-10 text-white rounded-lg mt-20 font-oxanium'>Explore More</button>
-                    <button class="border border-white bg-transparent w-32 h-10 text-white rounded-lg ml-4 mt-20 font-oxanium">Create</button>
-
-
-                </div>
-
-                <div>
-                    <img className='w-96  ' src={firstImg} />
-                </div>
-            </div>
-
-
-
-        
-    )
+      </div>
+   
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
