611d65c06f75c1d88dcd35ac776824ad5dea5359
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-17 14:11:59 +0530

    Wallet &  hero changed

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import image1 from '../../assets/Wallet/firstImg.svg'
-import image2 from '../../assets/Wallet/secondImg.svg'
-import image3 from '../../assets/Wallet/thirdImg.svg'
-import image4 from '../../assets/Wallet/fourthImg.svg'
-import image5 from '../../assets/Wallet/fifthImg.svg'
+import React from 'react';
+import metamask from '../../assets/Wallet/metamask.svg'
+import binance from '../../assets/Wallet/binance.svg'
+import trustwallet from '../../assets/Wallet/trustwallet.svg'
+import alpha from '../../assets/Wallet/alpha.svg'
+import coingecko from '../../assets/Wallet/coingecko.svg'
 
-function Wallet() {
-  const imageUrls = [image1, image2, image3, image4, image5];
-  const names = ["Metamask", "Binance", "Trust wallet", "Alpha", "CoinGecko"]
+
+
+const walletData = [
+  { name: "Metamask", image: metamask },
+  { name: "Binance", image: binance },
+  { name: "Trust Wallet", image: trustwallet },
+  { name: "Alpha", image: alpha },
+  { name: "CoinGecko", image: coingecko }
+];
+
+const Wallets = () => {
   return (
     <div className='text-white mt-20 pl-24 pr-24 p-4'>
       <p className="text-center text-lg font-Lato font-normal capitalize break-words" style={{
@@ -15,28 +23,23 @@ function Wallet() {
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent'
       }}>
-        Trade with world’s most trusted and fastest wallets
+        Trade with the world’s most trusted and fastest wallets
       </p>
 
       <h1 className='text-white text-7xl font-oxanium font-medium capitalize break-word text-center mt-5'>wallets we support</h1>
 
-
-
       <div className="flex space-x-4 items-center justify-between mt-14">
-        {imageUrls.map((imageUrl, index) => (
-          <div key={index} className=" h-full rounded-lg bg-white bg-opacity-10 p-8">
+        {walletData.map((wallet, index) => (
+          <div key={index} className="h-full rounded-lg bg-white bg-opacity-10 p-8">
             <div className="w-40 h-40 rounded-full bg-white bg-opacity-10 p-4 mb-8">
-              <img className=" m-4 w-24 h-24 object-cover rounded-full " src={imageUrl} />
+              <img className="m-4 w-24 h-24 object-cover rounded-full" src={wallet.image} alt={wallet.name} />
             </div>
-            <p className=" text-center font-oxanium capitalize break-words">{names[index]}</p>
+            <p className="text-center font-oxanium capitalize break-words">{wallet.name}</p>
           </div>
         ))}
       </div>
-
     </div>
-
-
-  )
+  );
 }
 
-export default Wallet
+export default Wallets;
