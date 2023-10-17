f3036177b8fe2685b9ffdc15066e1ef0da36b45a
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-17 14:34:37 +0530

    Wallet and hero destructured

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import metamask from '../../assets/Wallet/metamask.svg'
+import metaMask from '../../assets/Wallet/metaMask.svg'
 import binance from '../../assets/Wallet/binance.svg'
-import trustwallet from '../../assets/Wallet/trustwallet.svg'
+import trustWallet from '../../assets/Wallet/trustWallet.svg'
 import alpha from '../../assets/Wallet/alpha.svg'
-import coingecko from '../../assets/Wallet/coingecko.svg'
+import coinGecko from '../../assets/Wallet/coinGecko.svg'
 
 
 
 const walletData = [
-  { name: "Metamask", image: metamask },
+  { name: "Metamask", image: metaMask },
   { name: "Binance", image: binance },
-  { name: "Trust Wallet", image: trustwallet },
+  { name: "Trust Wallet", image: trustWallet },
   { name: "Alpha", image: alpha },
-  { name: "CoinGecko", image: coingecko }
+  { name: "CoinGecko", image: coinGecko }
 ];
 
 const Wallets = () => {
@@ -29,12 +28,12 @@ const Wallets = () => {
       <h1 className='text-white text-7xl font-oxanium font-medium capitalize break-word text-center mt-5'>wallets we support</h1>
 
       <div className="flex space-x-4 items-center justify-between mt-14">
-        {walletData.map((wallet, index) => (
+        {walletData.map(({name,image}, index) => (
           <div key={index} className="h-full rounded-lg bg-white bg-opacity-10 p-8">
             <div className="w-40 h-40 rounded-full bg-white bg-opacity-10 p-4 mb-8">
-              <img className="m-4 w-24 h-24 object-cover rounded-full" src={wallet.image} alt={wallet.name} />
+              <img className="m-4 w-24 h-24 object-cover rounded-full" src={image} alt={name} />
             </div>
-            <p className="text-center font-oxanium capitalize break-words">{wallet.name}</p>
+            <p className="text-center font-oxanium capitalize break-words">{name}</p>
           </div>
         ))}
       </div>
