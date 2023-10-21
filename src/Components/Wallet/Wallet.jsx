1882678cbd5f25bd4535e342f6ef557e7c672806
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-10-21 19:24:05 +0530

    Wallet changed

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,37 +1,35 @@
-import metaMask from '../../assets/Wallet/metaMask.svg'
-import binance from '../../assets/Wallet/binance.svg'
-import trustWallet from '../../assets/Wallet/trustWallet.svg'
-import alpha from '../../assets/Wallet/alpha.svg'
-import coinGecko from '../../assets/Wallet/coinGecko.svg'
-
-
+import metaMask from '../../assets/Wallet/metaMask.svg';
+import binance from '../../assets/Wallet/binance.svg';
+import trustWallet from '../../assets/Wallet/trustWallet.svg';
+import alpha from '../../assets/Wallet/alpha.svg';
+import coinGecko from '../../assets/Wallet/coinGecko.svg';
 
 const walletData = [
   { name: "Metamask", image: metaMask },
   { name: "Binance", image: binance },
   { name: "Trust Wallet", image: trustWallet },
   { name: "Alpha", image: alpha },
-  { name: "CoinGecko", image: coinGecko }
+  { name: "CoinGecko", image: coinGecko },
 ];
 
 const Wallets = () => {
   return (
-    <div className='text-white mt-20 pl-24 pr-24 p-4'>
+    <div className="text-white mt-20 pl-24 pr-24 p-4">
       <p className="text-center text-lg font-Lato font-normal capitalize break-words" style={{
         background: 'linear-gradient(to right, #FFFFFF, #F81DFB)',
         WebkitBackgroundClip: 'text',
-        WebkitTextFillColor: 'transparent'
+        WebkitTextFillColor: 'transparent',
       }}>
         Trade with the world’s most trusted and fastest wallets
       </p>
 
-      <h1 className='text-white text-7xl font-oxanium font-medium capitalize break-word text-center mt-5'>wallets we support</h1>
+      <h1 className="text-white text-7xl font-oxanium font-medium capitalize break-words text-center mt-5">wallets we support</h1>
 
       <div className="flex space-x-4 items-center justify-between mt-14">
-        {walletData.map(({name,image}, index) => (
-          <div key={index} className="h-full rounded-lg bg-white bg-opacity-10 p-8">
-            <div className="w-40 h-40 rounded-full bg-white bg-opacity-10 p-4 mb-8">
-              <img className="m-4 w-24 h-24 object-cover rounded-full" src={image} alt={name} />
+        {walletData.map(({ name, image }, index) => (
+          <div key={index} className="h-full rounded-lg bg-white bg-opacity-10 p-8 cursor-pointer group hover:scale-110 transition-transform duration-300 ease-in-out">
+            <div className="w-40 h-40 rounded-full bg-white bg-opacity-10 p-4 mb-8 relative">
+              <img className="m-4 w-24 h-24 object-cover rounded-full group-hover:scale-110 transition-transform duration-300 ease-in-out" src={image} alt={name} />
             </div>
             <p className="text-center font-oxanium capitalize break-words">{name}</p>
           </div>
